b03b5914aaa7cb2ac278d22d37e5d8d6711abbea
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-09 09:19:26 +0100

    handled documentations in the console.py module

--- console.py
+++ console.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""Implement the command line"""
+"""A module that implements the command line"""
 
 import cmd
 import sys
 
+
 class HbnbCommand(cmd.Cmd):
     """Implementation of the command line
-    intepreter
-    """
-    cmd.Cmd.prompt = "(hbnb) "
+        intepreter"""
+    cmd.Cmd.prompt = "(hbnb)"
 
     def do_quit(self, line):
+        """exit handler for the cmd loop"""
         sys.exit()
 
     def help_quit(self):
+        """provides guide for the quit command"""
         print("Quit command to exit the program")
 
     def do_EOF(self, line):
+        """handles the CTRL ^D"""
         return True
-
-
 
 
 if __name__ == "__main__":
